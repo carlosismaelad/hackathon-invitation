c1a5a052ff285dd3dad2831be9370f77fb72cac3
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-18 00:55:11 -0300

    structure the home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           </h1>
         </div>
 
-        <div className="flex gap-5 items-stretch flex-col md: flex-row">
+        <div className="flex gap-5 items-stretch flex-col md:flex-row">
           <div className="flex-1 bg-gray-700 border-gray-600 rounded-2xl p-8 space-y-6">
             <div className="flex items-center justify-between">
               <h2 className="font-heading font-semibold text-gray-200 text-xl">
