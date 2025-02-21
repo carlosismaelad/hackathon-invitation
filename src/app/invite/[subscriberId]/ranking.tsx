2b30ac9979b53d42756d529002420ef8184f3046
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-20 22:32:34 -0300

    gets data from the access and ranking database

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,29 +1,57 @@
+import { getRanking } from '@/http/api'
 import Image from 'next/image'
 import medalCooper from '../../../assets/medal-cooper.svg'
 import medalGold from '../../../assets/medal-gold.svg'
 import medalSilver from '../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
         Ranking de indicações
       </h2>
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1°</span> | Arthur André
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-300 leading-none">
-            1025
-          </span>
-          <Image
-            src={medalGold}
-            alt="gold medal"
-            className="absolute top-0 right-8"
-          />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}°</span> |{' '}
+                {item.name}
+              </span>
+              <span className="font-heading text-2xl font-semibold text-gray-300 leading-none">
+                {item.score}
+              </span>
+              {rankingPosition === 1 && (
+                <Image
+                  src={medalGold}
+                  alt="gold medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 2 && (
+                <Image
+                  src={medalSilver}
+                  alt="silver medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 1 && (
+                <Image
+                  src={medalCooper}
+                  alt="cooper medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
+        {/* <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
           <span className="text-sm text-gray-300 leading-none">
             <span className="font-semibold">2°</span> | Marcio Rennan
           </span>
@@ -48,7 +76,7 @@ export function Ranking() {
             alt="gold medal"
             className="absolute top-0 right-8"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,11 +1,24 @@
+import {
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+  getSubscriberRankingPosition,
+} from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
 
-export async function Stats() {
+interface StatsProps {
+  subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } = await getSubscriberInviteClicks(subscriberId)
+  const { count: inviteCount } = await getSubscriberInviteCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscriberRankingPosition(subscriberId)
   return (
     <div className="grid gap-3 md:grid-cols-3">
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {accessCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Acessos ao link
@@ -14,7 +27,7 @@ export async function Stats() {
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          746
+          {inviteCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Inscrições feitas
@@ -23,7 +36,7 @@ export async function Stats() {
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          3°
+          {rankingPosition ? `${rankingPosition}°` : '-'}°
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Posição no ranking
